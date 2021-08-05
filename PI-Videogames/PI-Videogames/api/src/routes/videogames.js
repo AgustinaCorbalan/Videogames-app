853796200f595703481f5ef76504cba493061fac
Agus Corbalan <tinicorb@gmail.com>
2021-08-04 23:56:39 -0300

    3er commit

--- PI-Videogames/PI-Videogames/api/src/routes/videogames.js
+++ PI-Videogames/PI-Videogames/api/src/routes/videogames.js
@@ -9,9 +9,9 @@ const { API_KEY } = process.env;
 router.get("/", async (req, res) => {
   try {
     const data = await fetch(
-      `https://api.rawg.io/api/games?key=62b0261a7e2d481f8cd3030c73d88cbb&page=1&page_size=40`
+      `https://api.rawg.io/api/games?key=${API_KEY}&page=1&page_size=40`
     );
-    res.json(data);
+    res.json(data.results);
   } catch (err) {
     console.log(err);
   }
